2afce70a432e4516f1faaefd1298ad561a9c18c1
James Eldridge <jeldridge@turbosquid.com>
2024-10-09 12:44:56 -0500

    add fetch logic for discussions, refactor posts folder dir

--- src/app/discussion/[slug]/page.tsx
+++ src/app/discussion/[slug]/page.tsx
@@ -1,4 +1,6 @@
 import PostCreateForm from "@/components/posts/postCreateForm";
+import PostList from "@/components/posts/postList";
+import { fetchPostsByDiscussionSlug } from "@/db/queries/posts";
 
 interface DiscussionShowPageProps {
   params: {
@@ -15,10 +17,11 @@ export default function DiscussionShowPage({
     <div className="grid grid-cols-4 gap-4 p-4">
       <div className="col-span-3">
         <h1 className="text-2xl font-bold mb-2">{slug}</h1>
+        <PostList fetchData={() => fetchPostsByDiscussionSlug(params.slug)} />
       </div>
 
       <div>
-        <PostCreateForm />
+        <PostCreateForm slug={slug} />
       </div>
     </div>
   );
